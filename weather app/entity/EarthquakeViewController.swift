5ed7c0103b91f5b65565b2e30ebe723b72b87aac
yakupatici <yakupatc7312@gmail.com>
2024-09-07 14:51:22 +0300

    added new

---
+++ weather app/entity/EarthquakeViewController.swift
@@ -0,0 +1,14 @@
+
+//
+//  EarthquakeViewController.swift
+//  Hava Durumu Takip
+//
+//  Created by Yakup Atıcı on 4.08.2024.
+//
+import UIKit
+
+class EarthquakeViewController: UIViewController {
+    
+   
+    }
+
